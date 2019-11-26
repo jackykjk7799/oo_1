fbcc53adebfc3619740cf6ec1adbc6800cc95494
jackykjk7799 <55517833+jackykjk7799@users.noreply.github.com>
2019-11-26 15:38:48 +0800

    class 20191126 update

---
+++ Class20191126.java
@@ -0,0 +1,43 @@
+public class Class20191126 {
+	// arguments are passed using the text field below this editor   
+	public static void main(String[] args)   {     
+		Rectangle r = new Rectangle(4.0, 5.0);     
+		System.out.println(r.toString());     
+		Triangle t = new Triangle(3.0, 4.0);     
+		System.out.println(t.toString());   
+	} 
+}  
+
+interface Shape{   
+	double getArea(); 
+}  
+
+class Rectangle implements Shape{   
+	private double length;   
+	private double width;   
+	public Rectangle(double length, double width){     
+		this.length = length;     
+		this.width = width;   
+	}   
+	public double getArea(){   	
+		return length * width;   
+	}   
+	public String toString(){   	
+		return "This is Rectangle\nArea : " + String.valueOf(this.getArea()) + "\n";   
+	}  
+}  
+
+class Triangle implements Shape{   
+	private double base;   
+	private double height;   
+	public Triangle(double base, double height){     
+		this.base = base;     
+		this.height = height;   
+	}   
+	public double getArea(){   	
+		return base * height;   
+	}   
+	public String toString(){   	
+		return "This is Triangle\nArea : " + String.valueOf(this.getArea()) + "\n";   
+	}  
+}
